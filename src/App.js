1dd21ffabe4e783c99094d5eb4f4e2e92c30cfa4
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-15 12:13:06 +0000

    app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import {dataLists} from './component/Data'
+import List from './component/List';
+
+
+
 
 function App() {
+
+  const [birthdayLists, setBirthdayLists] = useState(dataLists)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-violet-200 w-full h-screen flex items-center justify-center ">
+<section className='bg-zinc-100 w-[30%] min-h-[10vh] p-5 rounded-md flex justify-start items-start gap-8 flex-col'>
+  <h2 className='font-medium text-xl '>{birthdayLists.length} birthdays today</h2>
+<List birthdayLists={birthdayLists} />
+<button onClick={()=>setBirthdayLists([])}
+className="bg-violet-300 w-full p-3 font-medium text-xl"
+> 
+Clear All
+</button>
+</section>
+
+
+
     </div>
   );
 }

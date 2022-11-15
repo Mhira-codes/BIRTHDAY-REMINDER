4876d1a7dc9cc5570fbc4f30189fe14cc45fcc1e
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-15 12:16:32 +0000

    image added

---
+++ src/component/Data.js
@@ -0,0 +1,50 @@
+import pic1 from '../assets/pic-1.png'
+import pic2 from '../assets/pic-2.png'
+import pic3 from '../assets/pic-3.png'
+import pic4 from '../assets/pic-4.png'
+import pic5 from '../assets/pic-1.png'
+import pic6 from '../assets/pic-3.png'
+
+
+
+export const dataLists = [
+  {
+    id: 1,
+    name:"Mhira Alhassan",
+    age:32,
+    image: pic1
+  },
+  {
+    id: 2,
+    name:"Hajara Yussif",
+    age:12,
+    image: pic2
+  },
+  {
+    id: 3,
+    name:"Hannah Achampong",
+    age:20,
+    image: pic3,
+  },
+  {
+    id: 4,
+    name:"Kevin Benin",
+    age:32,
+    image: pic4
+  },
+  {
+    id: 5,
+    name:"Annie Quainoo",
+    age:18,
+    image: pic5
+  },
+  {
+    id: 6,
+    name:"Ali Umar",
+    age:23,
+    image: pic6
+  },
+ 
+
+
+];
